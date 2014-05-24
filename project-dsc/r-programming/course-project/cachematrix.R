c44e8917bc859a8483c2f643ea5529c7b8f027e1
Karthik Sonti <satyakarthik.sonti@gmail.com>
2014-05-24 15:04:09 -0400

    course-project

--- project-dsc/r-programming/course-project/cachematrix.R
+++ project-dsc/r-programming/course-project/cachematrix.R
@@ -1,15 +1,77 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix provides the setters and getters for the input matrix and its inverted version
+## cacheMatrix computes  the inverted versions of a given matrix if a cahced version is not available 
 
-## Write a short comment describing this function
+
+## makeCacheMatrix provides the setters and getters for the input matrix and its inverted version
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv.matrix <- NULL
+  
+  set <- function(initial.matrix) {
+    x <<- initial.matrix
+     ## whenever a matrix is changed . The cached inverse version should be set to null
+    inv.matrix <<- NULL
+    
+  }
+  
+  get <- function() {
+      return(x)
+  }
+  
+  setinverse <- function(inv) {
+      inv.matrix <<- inv
+  }
+  
+  getinverse <- function(){
+    return(inv.matrix)
+    
+  } 
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## The function cachesolve returns the inverse of the matrix cacheed.
+## Before computing the inverse, It will first check if there is a cached version available
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("returning cached version")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data) ## computing the inverse
+  x$setinverse(inv)
+  return(inv); ## returning the computed version
 }
+
+######################
+## unit tests : ######
+######################
+
+# m.test <-  matrix(c(2,4,1/2,1/4),2);
+#  m.fun <-  makeCacheMatrix(m.test)
+
+#> m.fun$get()
+#[,1] [,2]
+#[1,]    2 0.50
+#[2,]    4 0.25
+
+
+
+#first time invocation :
+#> cacheSolve(m.fun)
+#[,1]       [,2]
+#[1,] -0.1666667  0.3333333
+#[2,]  2.6666667 -1.3333333
+
+#second time invocation :
+##> cacheSolve(m.fun)
+##returning cached version
+#[,1]       [,2]
+#[1,] -0.1666667  0.3333333
+#[2,]  2.6666667 -1.3333333
